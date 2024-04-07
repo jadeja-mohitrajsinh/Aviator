4f04502ebd5e23b0f8ad95202ebb6ad0346b7902
king-mohitrajsinh <131246710+king-mohitrajsinh@users.noreply.github.com>
2024-04-07 21:49:25 +0530

    Update dbcon.php

--- database/dbcon.php
+++ database/dbcon.php
@@ -16,10 +16,10 @@ use Kreait\Firebase\ServiceAccount;
 
 
 
-$baseStorageUrl = 'https://firebasestorage.googleapis.com/v0/b/aviator-a150a.appspot.com/o/images%2F';
+$baseStorageUrl = 'https://firebasestorage.googleapis.com/v0/b/your-project-name.appspot.com';//replace with the storage url
 // Firebase credentials
-$firebaseCredentialPath ='DATABASE/aviator-a150a-firebase-adminsdk-lhv3p-8bfc4c3fc4.json';
-$firebaseDatabaseUri = 'https://aviator-a150a-default-rtdb.firebaseio.com';
+$firebaseCredentialPath ='DATABASE/__path to json__';
+$firebaseDatabaseUri = ')(https://(link)rtdb.firebaseio.co))';//replace with your project link
 
 // Firebase initialization using Factory
 $firebase = (new Factory())
